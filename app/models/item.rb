1717ec76cedf66e0b0daf29266e55cd215cc0b7f
Drew & Paul Morin <student@epicodus.com>
2015-07-28 13:58:00 -0700

    clean up before branching

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ActiveRecord::Base
   validates :name, presence: true
   belongs_to :category
-  
+
+  # def votes
+  #   votes_for = Vote.where(category: self.category, voted_up_item_id: self.id).count
+  #   votes_against = Vote.where(category: self.category, voted_down_item_id: self.id).count
+  #   return votes_for - votes_against
+  # end
+
 end

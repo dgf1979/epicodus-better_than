0b8b0571056c5d6f61762fee9f7cc50765a81b48
Drew <student@epicodus.com>
2015-07-30 15:09:33 -0700

    randomize item ratings and increase number of initial categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 bridge_category = Category.create({ name: "PDX Bridges" })
-bridges = ['Morrison', 'Steel', 'Burnside', 'Broadway', 'Hawthorne']
+bridges = ['Morrison', 'Steel', 'Burnside', 'Broadway', 'Hawthorne', 'St Johns', 'Fremont', 'Sellwood']
 bridges.each do |bridge|
-  bridge_category.items.create({ name: bridge })
+  bridge_category.items.create({ name: bridge, rank: rand(90..110) })
 end
 
 food_cart_category = Category.create({ name: "Food Carts" })
-food_carts = ['The Frying Scottsman', '808 Grinds', 'Guero', 'Viking Soul Food', "Cackalack's Hot Chicken Shack" ]
+food_carts = ['The Frying Scottsman', '808 Grinds', 'Guero', 'Viking Soul Food', "Cackalack's Hot Chicken Shack", "Potato Chmapion", "Fried Eggs I'm In Love", "Pyro Pizza" ]
 food_carts.each do |food_cart|
-  food_cart_category.items.create({ name: food_cart })
+  food_cart_category.items.create({ name: food_cart, rank: rand(90..110) })
+end
+
+brewery_category = Category.create({ name: "Local Brewery" })
+breweries = ['Deschutes', 'Burnside', 'Cascade', 'Hopworks', 'Breakside', 'Laurelwood', 'Base Camp', 'Rock Bottom', 'Gigantic']
+breweries.each do |brewery|
+  brewery_category.items.create({ name: brewery, rank: rand(90..110) })
+end
+
+theater_category = Category.create({ name: "Movie Theater" })
+theaters = ['Hollywood', 'Laurelhurst', 'Cinema 21', 'Bagdad', 'Kennedy School', 'Regal Fox Tower', 'Living Room', 'Mission']
+theaters.each do |theater|
+  theater_category.items.create({ name: theater, rank: rand(90..110) })
+end
+
+movie_category = Category.create({ name: "Movie of 2015" })
+movies = ['Mad Max: Fury Road', 'Jurassic World', 'Avengers: Age of Ultron', 'Furious 7', 'Ex Machina', 'Magic Mike XXL', 'Pitch Perfect 2']
+movies.each do |movie|
+  movie_category.items.create({ name: movie, rank: rand(90..110) })
 end
